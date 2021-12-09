5f44818993dd0f8ece544cf00f916dc754ee90a4
xovest <79008917+xovest@users.noreply.github.com>
2021-12-09 17:55:34 +0200

    multiple qrs

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@
   siteMetadata: {
     title: 'Web Warrior',
     description: 'web dev portfolio',
-    copyright: 'This website is copyright 2021 Web Warrior'
+    copyright: 'This website is copyright 2021 Web Warrior',
+    contact: 'me@thewebwarrioruk.co.uk',
   },
 }
