8079fe339cef189ebb16ef28fd493cf9c2ed46c2
xovest <79008917+xovest@users.noreply.github.com>
2021-12-09 17:33:25 +0200

    graph queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,12 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
+ module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Web Warrior',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright 2021 Web Warrior'
+  },
 }
